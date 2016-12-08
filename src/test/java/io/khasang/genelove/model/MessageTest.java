f4568986a0ac098781bbdca0df488981a8fe3bb7
Kuznetsov Sergey <azon.sk@gmail.com>
2016-12-08 21:21:24 +0300

    add content to messagetest

--- src/test/java/io/khasang/genelove/model/MessageTest.java
+++ src/test/java/io/khasang/genelove/model/MessageTest.java
@@ -1,4 +1,12 @@
 package io.khasang.genelove.model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MessageTest {
+
+    @Test
+    public void testGetMessageWithParam(){
+        Assert.assertEquals("Hello world", "Hello world");
+    }
 }
