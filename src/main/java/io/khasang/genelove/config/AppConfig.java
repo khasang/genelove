8919caf239b79ae7f62032f8e8f60a0c2129f511
Kuznetsov Sergey <azon.sk@gmail.com>
2016-12-08 22:12:04 +0300

    add beans to our programm

---
+++ src/main/java/io/khasang/genelove/config/AppConfig.java
@@ -0,0 +1,14 @@
+package io.khasang.genelove.config;
+
+import io.khasang.genelove.model.Message;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Message message(){
+        return new Message("Hello my bean");
+    }
+}
