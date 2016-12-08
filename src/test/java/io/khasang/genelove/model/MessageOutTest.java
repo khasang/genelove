4a0bf685f10f9ed3568448a44df593750a136bc0
Kuznetsov Sergey <azon.sk@gmail.com>
2016-12-08 21:25:18 +0300

    add content to out message test class

--- src/test/java/io/khasang/genelove/model/MessageOutTest.java
+++ src/test/java/io/khasang/genelove/model/MessageOutTest.java
@@ -1,7 +1,14 @@
 package io.khasang.genelove.model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /*
 * Class for testing out message
 * */
 public class MessageOutTest {
+    @Test
+    public void testGetMessageWithParam(){
+        Assert.assertEquals("Hello world", "Hello world");
+    }
 }
