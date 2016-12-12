becce0e6ccef630c7d96f4005b437c0c6868b0fb
Kuznetsov Sergey <azon.sk@gmail.com>
2016-12-12 22:02:28 +0300

    add spring jdbc example

--- src/main/java/io/khasang/genelove/controller/AppController.java
+++ src/main/java/io/khasang/genelove/controller/AppController.java
@@ -1,5 +1,6 @@
 package io.khasang.genelove.controller;
 
+import io.khasang.genelove.model.CreateTable;
 import io.khasang.genelove.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AppController {
     @Autowired
     Message message;
+    @Autowired
+    CreateTable createTable;
 
     @RequestMapping("/")
     public String hello(Model model){
         model.addAttribute("hello", message.getMessageOut());
         return "hello";
+    }
+
+    @RequestMapping("/create")
+    public String createTable(Model model){
+        model.addAttribute("create", createTable.createTableStatus());
+        return "create";
     }
 }
