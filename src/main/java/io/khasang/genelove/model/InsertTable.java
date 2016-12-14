01221fb67c1a6c31a4676a3bb576f81c08337f2b
bigud <basily@bk.ru>
2016-12-14 21:53:43 +0300

    тест

--- src/main/java/io/khasang/genelove/model/InsertTable.java
+++ src/main/java/io/khasang/genelove/model/InsertTable.java
@@ -1,7 +1,34 @@
 package io.khasang.genelove.model;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Created by Vasily on 14.12.2016.
  */
 public class InsertTable {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public InsertTable(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public InsertTable() {
+    }
+
+    private String insert() {
+        try {
+            jdbcTemplate.execute("INSERT INTO films (code, title, did, date_prod, kind) VALUES('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy'), ('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');");
+            return "Data inserted";
+        } catch (Exception e) {
+            return "Insert failed: " + e;
+        }
+    }
+
+    public String insertTableStatus() {
+        return insert();
+    }
+
+
+
 }
