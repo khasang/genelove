c84c91c0f9827f7f27d10956a3a2a4e297269d9e
bigud <basily@bk.ru>
2016-12-14 21:54:37 +0300

    тест

--- src/main/java/io/khasang/genelove/controller/AppController.java
+++ src/main/java/io/khasang/genelove/controller/AppController.java
@@ -1,5 +1,8 @@
 package io.khasang.genelove.controller;
 
+import io.khasang.genelove.model.InsertTable;
+import io.khasang.genelove.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
 
-    @RequestMapping("/")
-    public String hello(Model model){
-        model.addAttribute("goodbay", "Goodbay World");
-        return "goodbay";
-    }
+
+        @Autowired
+        Message message;
+        @Autowired
+        InsertTable insertTable;
+
+        @RequestMapping("/")
+        public String hello(Model model){
+            model.addAttribute("hello", message.getMessageOut());
+            return "hello";
+        }
+
+        @RequestMapping("/insert")
+        public String insertTable(Model model){
+            model.addAttribute("create", insertTable.insertTableStatus());
+            return "insert";
+        }
+
 }
