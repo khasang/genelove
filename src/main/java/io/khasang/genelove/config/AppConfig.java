3e0f7b97f018df58de5694b1dff81c2f4133ec4f
Alexander Pyankov <python239@mail.ru>
2016-12-18 01:26:53 +0300

    add create and insert

--- src/main/java/io/khasang/genelove/config/AppConfig.java
+++ src/main/java/io/khasang/genelove/config/AppConfig.java
@@ -1,7 +1,50 @@
-package io.khasang.genelove.config.application;
+package io.khasang.genelove.config;
 
-/**
- * Created by alex on 17.12.16.
- */
+import io.khasang.genelove.model.CreateTable;
+import io.khasang.genelove.model.InsertTable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import io.khasang.genelove.model.Message;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource(value = {"classpath:util.properties"})
 public class AppConfig {
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public Message message() {
+        return new Message("Hello my Bean!");
+    }
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.postgresql.driverClass"));
+        dataSource.setUrl(environment.getProperty("jdbc.postgresql.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.postgresql.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.postgresql.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public CreateTable createTable() {
+        return new CreateTable(jdbcTemplate());
+    }
+    @Bean
+    public InsertTable insertTable() {
+        return new InsertTable(jdbcTemplate());
+    }
 }
