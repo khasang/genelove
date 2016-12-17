c1630d79c4dd8490b3e871d832462618ab78aed9
Alexander Pyankov <python239@mail.ru>
2016-12-18 02:17:49 +0300

    add simple Select SQL-statement (author ruslanLagai)

--- src/main/java/io/khasang/genelove/model/SimpleSelect.java
+++ src/main/java/io/khasang/genelove/model/SimpleSelect.java
@@ -1,7 +1,33 @@
 package io.khasang.genelove.model;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Created by alex on 18.12.16.
  */
 public class SimpleSelect {
+    private JdbcTemplate jdbcTemplate;
+
+    public SimpleSelect(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public SimpleSelect() {
+    }
+
+    private String simpleSelect () {
+        try {
+            String result = jdbcTemplate.queryForObject
+                    ("SELECT title FROM films WHERE did = (SELECT max(did) FROM films)", String.class);
+            return "result: " + result.toString();
+        }
+        catch (Exception e) {
+            return "Data simple select into films failed: " + e;
+        }
+
+    }
+
+    public String simpleSelectTableStatus () {
+        return simpleSelect();
+    }
 }
