1a2b878b922cf49c8757c88a8c5e4edc51587118
Alexander Pyankov <python239@mail.ru>
2016-12-18 01:25:36 +0300

    add create statemment

--- src/main/java/io/khasang/genelove/model/CreateTable.java
+++ src/main/java/io/khasang/genelove/model/CreateTable.java
@@ -1,7 +1,38 @@
 package io.khasang.genelove.model;
 
-/**
- * Created by alex on 17.12.16.
- */
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class CreateTable {
+    private JdbcTemplate jdbcTemplate;
+
+    public CreateTable (JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public CreateTable() {
+    }
+
+    private String create () {
+        try {
+            jdbcTemplate.execute ("drop table if exists films");
+            jdbcTemplate.execute ("CREATE TABLE films (\n" +
+                    "    code        char(5),\n" +
+                    "    title       varchar(40),\n" +
+                    "    did         integer,\n" +
+                    "    date_prod   date,\n" +
+                    "    kind        varchar(10),\n" +
+                    "    len         interval hour to minute,\n" +
+                    "    CONSTRAINT production UNIQUE(date_prod)\n" +
+                    ");");
+            return "Table created";
+        }
+        catch (Exception e) {
+            return "Table creation failed: " + e;
+        }
+
+    }
+
+    public String createTableStatus () {
+        return create();
+    }
 }
