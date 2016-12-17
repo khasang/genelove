542c4403c6f2aef209643a084924150195ce758b
Alexander Pyankov <python239@mail.ru>
2016-12-18 01:26:26 +0300

    add create and insert

--- src/main/java/io/khasang/genelove/model/Message.java
+++ src/main/java/io/khasang/genelove/model/Message.java
@@ -1,7 +1,21 @@
 package io.khasang.genelove.model;
 
-/**
- * Created by alex on 17.12.16.
- */
+
 public class Message {
+    private String messageOut;
+
+    public Message(String messageOut) {
+        this.messageOut = messageOut;
+    }
+
+    public Message() {
+    }
+
+    public String getMessageOut() {
+        return messageOut;
+    }
+
+    public void setMessageOut(String messageOut) {
+        this.messageOut = messageOut;
+    }
 }
