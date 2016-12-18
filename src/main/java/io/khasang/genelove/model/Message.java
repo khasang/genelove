9d7d6147627c2828009613534bc14d2394c4398c
Dmitry <DGatilov65@gmail.com>
2016-12-18 18:29:27 +0300

    Add content to message

--- src/main/java/io/khasang/genelove/model/Message.java
+++ src/main/java/io/khasang/genelove/model/Message.java
@@ -3,6 +3,7 @@ package io.khasang.genelove.model;
 import org.springframework.stereotype.Component;
 
 // Created by DGatilov on 17.12.2016.
+// Add comment
 @Component
 public class Message {
     private String messageOut;
