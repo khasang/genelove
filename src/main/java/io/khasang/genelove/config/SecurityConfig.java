f94a271eb6f73afe512eac92378f0ad87831f815
Kuznetsov Sergey <azon.sk@gmail.com>
2016-12-18 21:39:36 +0300

    add spring security from db

--- src/main/java/io/khasang/genelove/config/SecurityConfig.java
+++ src/main/java/io/khasang/genelove/config/SecurityConfig.java
@@ -1,28 +1,40 @@
 package io.khasang.genelove.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .antMatchers("/admin/**").access("hasAnyRole('ADMIN','USER')")
+                .antMatchers("/db/**").access("hasRole('DB')")
                 .and().csrf().disable().formLogin().defaultSuccessUrl("/", false);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("adm").password("adm").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("superadmin").password("superadmin").roles("SUPERADMIN");
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/io/khasang/genelove/controller/AppController.java
+++ src/main/java/io/khasang/genelove/controller/AppController.java
@@ -3,9 +3,13 @@ package io.khasang.genelove.controller;
 import io.khasang.genelove.model.CreateTable;
 import io.khasang.genelove.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -14,15 +18,24 @@ public class AppController {
     @Autowired
     CreateTable createTable;
 
-    @RequestMapping("/")
-    public String hello(Model model){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello(Model model) {
         model.addAttribute("hello", message.getMessageOut());
         return "hello";
     }
 
-    @RequestMapping("/admin/create")
-    public String createTable(Model model){
+    @RequestMapping(value = "/admin/create", method = RequestMethod.GET)
+    public String createTable(Model model) {
         model.addAttribute("create", createTable.createTableStatus());
         return "create";
+    }
+
+    @RequestMapping(value = {"hello/{name}"}, method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("encode");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(name));
+        return modelAndView;
+
     }
 }
