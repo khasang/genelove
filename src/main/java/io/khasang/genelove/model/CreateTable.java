8f2508d9be6d57f5178b7820e8e4945d34409a5a
Dmitry <DGatilov65@gmail.com>
2016-12-24 16:33:48 +0300

    Modify content in block try ... catch

--- src/main/java/io/khasang/genelove/model/CreateTable.java
+++ src/main/java/io/khasang/genelove/model/CreateTable.java
@@ -31,14 +31,22 @@ public class CreateTable {
         try {
             jdbcTemplate.execute("drop table if exists params");
 
-            jdbcTemplate.execute("CREATE TABLE params (param_id integer NOT NULL CONSTRAINT pk_param_id PRIMARY KEY ,n" +
+            jdbcTemplate.execute("CREATE TABLE params (param_id integer NOT NULL CONSTRAINT pk_param_id PRIMARY KEY ,\n" +
                                                            "param_group_id integer NOT NULL,\n" +
                                                            "description varchar(255));");
-            return "Table films and params created";
+            //return "Table films and params created";
         } catch (Exception e) {
             return "Table params creation failed: " + e;
         }
 
+        try {
+            jdbcTemplate.execute("drop table if exists params_groups");
+            jdbcTemplate.execute("CREATE TABLE params_groups (param_group_id integer NOT NULL CONSTRAINT pk_param_group_id PRIMARY KEY ,\n" +
+                                      "description varchar(255));");
+            return "Table films ,params & params_groups created";
+        } catch (Exception e) {
+            return "Table params creation failed: " + e;
+        }
 
     }
 
