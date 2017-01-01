082b9cbee81f981f2356b66e847751efa013ed28
Dmitry <DGatilov65@gmail.com>
2017-01-01 14:57:42 +0300

    modify Question.java

--- src/main/java/io/khasang/genelove/entity/Question.java
+++ src/main/java/io/khasang/genelove/entity/Question.java
@@ -41,3 +41,49 @@ public class Question {
     public String getQuestion() {
         return question;
     }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(String correectAnswers) {
+        this.correctAnswers = correectAnswers;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
+    }
+
+}
