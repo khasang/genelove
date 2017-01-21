494d12e895439c2083a85fcdd1f24dc74704125f
Alexander Pyankov <python239@mail.ru>
2017-01-21 20:05:24 +0300

    w2

--- src/main/java/io/khasang/genelove/dao/SQLServiceDAO.java
+++
@@ -1,7 +0,0 @@
-package io.khasang.genelove.dao;
-
-/**
- * Created by alex on 21.01.17.
- */
-public class SQLServiceDAO {
-}
--- src/main/java/io/khasang/genelove/service/SQLService.java
+++
@@ -1,7 +0,0 @@
-package io.khasang.genelove.service;
-
-/**
- * Created by alex on 21.01.17.
- */
-public class SQLService {
-}
